f21ec1489d22944ed1bb924cc20bc08b39efdd3e
Roger Peppe <rogpeppe@gmail.com>
2018-04-19 19:29:05 +0100

    add code

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # godate
+
 A simple command to print dates with Go-style formatting
+
+Usage: godate [flags] [time...]
+
+## Flags
+
+- -f *string*
+    	use Go-style time format string (or name) (default "rfc3339nano")
+- -in *string*
+    	interpret argument time as this Go-style format (or name) (default "unix")
+- -l
+	print local time (default is UTC)
+
+By default rfcdate prints the current time in RFC3339 format in
+the UTC time zone. The -f flag can be used to change the format
+that is printed (see https://golang.org/pkg/time/#Time.Format
+for details). The reference date is:
+
+	Mon Jan 2 15:04:05 -0700 MST 2006
+
+The format may be the name of one of the predefined format
+constants in the time package (case-insensitive), in which case that format will be used.
+The supported formats are these:
+
+    ansic       Mon Jan _2 15:04:05 2006
+    go          2006-01-02 15:04:05.999999999 -0700 MST
+    kitchen     3:04PM
+    rfc1123     Mon, 02 Jan 2006 15:04:05 MST
+    rfc1123z    Mon, 02 Jan 2006 15:04:05 -0700
+    rfc3339     2006-01-02T15:04:05Z07:00
+    rfc3339nano 2006-01-02T15:04:05.999999999Z07:00
+    rfc822      02 Jan 06 15:04 MST
+    rfc822z     02 Jan 06 15:04 -0700
+    rfc850      Monday, 02-Jan-06 15:04:05 MST
+    rubydate    Mon Jan 02 15:04:05 -0700 2006
+    stamp       Jan _2 15:04:05
+    stampmicro  Jan _2 15:04:05.000000
+    stampmilli  Jan _2 15:04:05.000
+    stampnano   Jan _2 15:04:05.000000000
+    unix        custom
+    unixdate    Mon Jan _2 15:04:05 MST 2006
+    unixnano    custom
+
+The unix and unixnano formats are special cases that print the number of seconds
+or nanoseconds since the Unix epoch (Jan 1st 1970). The "go" format is the
+format used by the time package to print times by default.
+
+When one or more arguments are provided, they will be used as the time
+to print instead of the current time. The -in flag can be used to specify
+what format to interpret these arguments in. Again, unix and unixnano
+can be used to specify input in seconds or nanoseconds since the Unix epoch.
---
+++ godate.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+	"time"
+)
+
+var (
+	local  = flag.Bool("l", false, "print local time (default is UTC)")
+	format = flag.String("f", "rfc3339nano", "use Go-style time format string (or name)")
+	in     = flag.String("in", "unix", "interpret argument time as this Go-style format (or name)")
+)
+
+var knownFormats = map[string]string{
+	"ansic":       time.ANSIC,
+	"unixdate":    time.UnixDate,
+	"rubydate":    time.RubyDate,
+	"rfc822":      time.RFC822,
+	"rfc822z":     time.RFC822Z,
+	"rfc850":      time.RFC850,
+	"rfc1123":     time.RFC1123,
+	"rfc1123z":    time.RFC1123Z,
+	"rfc3339":     time.RFC3339,
+	"rfc3339nano": time.RFC3339Nano,
+	"kitchen":     time.Kitchen,
+	"stamp":       time.Stamp,
+	"stampmilli":  time.StampMilli,
+	"stampmicro":  time.StampMicro,
+	"stampnano":   time.StampNano,
+	"go":          "2006-01-02 15:04:05.999999999 -0700 MST",
+	"unix":        "custom",
+	"unixnano":    "custom",
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+	var times []time.Time
+	if flag.NArg() > 0 {
+		for _, arg := range flag.Args() {
+			t, err := parseTime(*in, arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "parse error: %v\n", err)
+				os.Exit(2)
+			}
+			times = append(times, t)
+		}
+	} else {
+		times = []time.Time{
+			time.Now(),
+		}
+	}
+	for _, t := range times {
+		if !*local {
+			t = t.UTC()
+		}
+		fmt.Printf("%s\n", formatTime(t, *format))
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, `
+Usage: godate [flags] [time...]
+Flags:
+`[1:])
+	flag.PrintDefaults()
+
+	fmt.Fprintf(os.Stderr, `
+
+By default rfcdate prints the current time in RFC3339 format in
+the UTC time zone. The -f flag can be used to change the format
+that is printed (see https://golang.org/pkg/time/#Time.Format
+for details). The reference date is:
+
+	Mon Jan 2 15:04:05 -0700 MST 2006
+
+The format may be the name of one of the predefined format
+constants in the time package (case-insensitive), in which case that format will be used.
+The supported formats are these:
+
+`[1:])
+	type format struct {
+		name   string
+		format string
+	}
+	var formats []format
+	for name, f := range knownFormats {
+		formats = append(formats, format{name, f})
+	}
+	sort.Slice(formats, func(i, j int) bool {
+		return formats[i].name < formats[j].name
+	})
+	w := tabwriter.NewWriter(os.Stderr, 4, 4, 1, ' ', 0)
+	for _, f := range formats {
+		fmt.Fprintf(w, "\t%s\t%s\n", f.name, f.format)
+	}
+	w.Flush()
+
+	fmt.Fprintf(os.Stderr, `
+
+The unix and unixnano formats are special cases that print the number of seconds
+or nanoseconds since the Unix epoch (Jan 1st 1970). The "go" format is the
+format used by the time package to print times by default.
+
+When one or more arguments are provided, they will be used as the time
+to print instead of the current time. The -in flag can be used to specify
+what format to interpret these arguments in. Again, unix and unixnano
+can be used to specify input in seconds or nanoseconds since the Unix epoch.
+`[1:])
+	os.Exit(2)
+}
+
+func parseTime(format string, s string) (time.Time, error) {
+	if format1, ok := knownFormats[strings.ToLower(format)]; ok {
+		if format1 == "custom" {
+			return parseUnix(format, s)
+		}
+		format = format1
+	}
+	return time.Parse(format, s)
+}
+
+func parseUnix(format, s string) (time.Time, error) {
+	ts, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return time.Time{}, err
+	}
+	switch format {
+	case "unix":
+		return time.Unix(ts, 0), nil
+	case "unixnano":
+		return time.Unix(0, ts), nil
+	default:
+		panic("unknown unix time format")
+	}
+}
+
+func formatTime(t time.Time, format string) string {
+	if format1, ok := knownFormats[strings.ToLower(format)]; ok {
+		if format1 == "custom" {
+			return formatUnix(t, format)
+		}
+		format = format1
+	}
+	return t.Format(format)
+}
+
+func formatUnix(t time.Time, format string) string {
+	switch format {
+	case "unix":
+		return fmt.Sprint(t.Unix())
+	case "unixnano":
+		return fmt.Sprint(t.UnixNano())
+	default:
+		panic("unknown unix time format")
+	}
+}
